65d2573cf5d960b2319ee5100f33d5ace3711b66
cutterpillow3 <148648353+cutterpillow3@users.noreply.github.com>
2025-04-19 23:47:15 +0800

    Update test_db.php

--- project_code/test_db.php
+++ project_code/test_db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new SQLite3('/Users/mau/Documents/travel-planner/database/travel.db'); // simplified connection.
+    $db = new SQLite3('travel-planner/database/travel.db'); // simplified connection.
     echo "Database connected successfully!";
     $db->close();
 } catch (Exception $e) {
