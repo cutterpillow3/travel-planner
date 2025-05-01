5433a1a596ef19f3fa5c8e8586986aebd9706df5
cutterpillow3 <148648353+cutterpillow3@users.noreply.github.com>
2025-05-02 05:28:05 +0800

    Update trip_planner.php

--- project_code/trip_planner.php
+++ project_code/trip_planner.php
@@ -11,10 +11,14 @@ if (!isset($_GET['destinations'])) {
 $destination_ids = explode(',', $_GET['destinations']);
 $destination_ids = array_map('intval', $destination_ids); // Sanitize input
 
-function get_transport_options($db, $from_id, $to_id) {
-    $stmt = $db->prepare('SELECT `to`.transport_type, `to`.description FROM transport_options `to` JOIN destination_transport dt ON `to`.transport_id = dt.transport_id WHERE dt.destination_id_from = :from AND dt.destination_id_to = :to');
-    $stmt->bindValue(':from', $from_id, SQLITE3_INTEGER);
-    $stmt->bindValue(':to', $to_id, SQLITE3_INTEGER);
+function get_transport_options_at_destination($db, $destination_id) {
+    $stmt = $db->prepare('
+        SELECT topt.transport_type, topt.description
+        FROM transport_options topt
+        JOIN destination_transport dt ON topt.transport_id = dt.transport_id
+        WHERE dt.destination_id = :destination_id
+    ');
+    $stmt->bindValue(':destination_id', $destination_id, SQLITE3_INTEGER);
     $results = $stmt->execute();
 
     $options = [];
@@ -25,7 +29,7 @@ function get_transport_options($db, $from_id, $to_id) {
 }
 
 function get_eco_rating($db, $destination_id) {
-    $stmt = $db->prepare('SELECT eco_rating FROM destinations WHERE destination_id = :id');
+    $stmt = $db->prepare('SELECT eco_rating, name FROM destinations WHERE destination_id = :id');
     $stmt->bindValue(":id", $destination_id, SQLITE3_INTEGER);
     $result = $stmt->execute();
     return $result->fetchArray(SQLITE3_ASSOC);
@@ -37,23 +41,15 @@ $trip_plan = [];
 $total_eco_rating = 0;
 
 // Loop through the selected destination IDs to get transport options and eco-ratings
-for($i = 0; $i < count($destination_ids) - 1; $i++){
-    $from_id = $destination_ids[$i];
-    $to_id = $destination_ids[$i+1];
-    $transport_options = get_transport_options($db, $from_id, $to_id);
-    $trip_plan[] = ["from" => $from_id, "to" => $to_id, "options" => $transport_options];
+foreach ($destination_ids as $destination_id) {
+    $transport_options = get_transport_options_at_destination($db, $destination_id);
+    $destination_info = get_eco_rating($db, $destination_id);
+    $destination_name = $destination_info ? $destination_info['name'] : 'Unknown Destination';
+    $trip_plan[] = ["destination_id" => $destination_id, "name" => $destination_name, "transport_options" => $transport_options];
 
-    // Get eco-rating for the starting destination
-    $destination = get_eco_rating($db, $from_id);
-    if ($destination) {
-        $total_eco_rating += $destination['eco_rating'];
+    if ($destination_info && isset($destination_info['eco_rating'])) {
+        $total_eco_rating += $destination_info['eco_rating'];
     }
-}
-
-// Get eco-rating for the final destination
-$destination = get_eco_rating($db, $destination_ids[count($destination_ids)-1]);
-if ($destination) {
-    $total_eco_rating += $destination['eco_rating'];
 }
 
 // Calculate the average eco-rating
